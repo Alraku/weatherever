7a647c8947d7aba0b717d6d808721bce2c7f9985
Alraku <ekoalraku@gmail.com>
2022-01-23 11:39:44 +0100

    Basic gathering weather info

---
+++ Weather Forecast/src/main.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+from pprint import pprint
+
+class Weather():
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?&appid=fafa5ec8ced374a06b103d3c60e1b76d&units=metric&lang=pl'
+        
+    def showWeather(self, city="Gdańsk"): 
+        self.city = city
+        r = requests.get(self.url + '&q=' + self.city)
+        weather = json.loads(r.text)
+        pprint(weather)
+
+# print(weather_data['city'])
+# print(weather_data.get('city').get('name'))
+# print(weather_data.get('city').get('main').get('temp'))
+
+test = Weather()
+test.showWeather('London')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# logger.debug("debug message")
+# logger.info("info message")
+# logger.warning("warning message")
+# logger.error("error message")
+# logger.critical("critical message")
